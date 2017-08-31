9e73649fdad7725b490cac5e523505a08170f407
Javaria <rajesh.javaria@on.nokia.com>
2017-09-01 00:35:32 +1000

    Evaluation

---
+++ src/main/java/au/com/inteliment/server/counter/environment/WebApplicationConfig.java
@@ -0,0 +1,31 @@
+package au.com.inteliment.server.counter.environment;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Read the environment properties..
+ * <p>
+ * Reading content file name from application.properties
+ * @author rjavaria
+ *
+ */
+@Configuration
+@ConfigurationProperties(ignoreUnknownFields = false,
+						 prefix = "counter.web.env")
+public class WebApplicationConfig extends WebMvcConfigurerAdapter {
+    
+	@Value("${counter.web.env.filename :content/paragraph.txt}")
+    private String filename;
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+}
